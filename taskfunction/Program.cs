1c937b30c86533f5641769ceb5316d57c45181f6
zeeen <zeeen@DESKTOP-GP9JGLO>
2024-12-18 14:56:34 +0300

    Add project files.

---
+++ taskfunction/Program.cs
@@ -0,0 +1,112 @@
+﻿#region AllFunction
+
+//task1
+string FullName(string name)
+{
+    return  name;
+}
+
+//task2
+ double volume( double width, double height, double length)
+{
+
+    return width * height * length;
+}
+
+
+//task3
+int number( int num)
+{
+
+    if (num % 2 == 0)
+    {
+        return num*2;
+    }
+    else
+    {
+        return num* num;
+    }
+}
+
+//task4
+double Celsius( double temp)
+{
+
+    return (temp * 9 / 5) + 32;
+}
+
+
+//task5
+string letters(char check){
+    if (check == 'a' || check == 'o' || check == 'u' || check == 'i' || check == 'e' )
+    {
+        return "vowel";
+            }
+
+    else
+    {
+        return "constant";
+    }
+
+}
+
+//task6
+double currency( double originalAmount, double conversionRate)
+{
+    double converstionAmount= originalAmount * conversionRate;
+    return converstionAmount;
+
+}
+
+
+#endregion
+
+
+
+
+
+
+
+
+
+
+#region Callfunction
+//task1
+//Console.WriteLine(" welcome " + Console.ReadLine());
+Console.WriteLine("enter your name ");
+string name = Console.ReadLine();
+Console.WriteLine("welcome    " + FullName(name));
+
+//task2
+Console.WriteLine(" enter values");
+double valueA =Convert.ToDouble(Console.ReadLine());
+double valueB =Convert.ToDouble(Console.ReadLine());
+double valueC =Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("the valume is:   " + volume(valueA, valueB, valueC));
+
+//task3
+Console.WriteLine("enter the number");
+int valueD = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("the number is :   " + number(valueD));
+
+//task4
+Console.WriteLine("enter the tempreture");
+double valueH = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("the tempreture is :  " +Celsius(valueH));
+
+//task5
+Console.WriteLine("enter a letter");
+char check = Convert.ToChar(Console.ReadLine());
+Console.WriteLine(letters(check));
+
+
+Console.WriteLine("enter amount");
+double originalAmount = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(" the convertion amount is :");
+double conversionRate = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(currency(originalAmount, conversionRate));
+
+
+#endregion region
+
+
